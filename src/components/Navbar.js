a8c0be6b8523df1aedd42d816012de7d0f1efaab
ELOUAIZI AYOUB <ayoubelouaizi@gmail.com>
2023-02-26 02:09:24 +0000

    using Locale storage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,39 +2,39 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './Navbar.css'
 export const Navbar = () => {
-  return (
-      <nav className='navbar'>
-         <ul>
-          <li>
-              <Link to="/">Home</Link>
-          </li>
-          <li>
-              <Link to="/login">Login</Link>
-          </li>
-          <li>
-              <Link to="/orgdash">OrganizerDashbord</Link>
-          </li>
-          <li>
-              <Link to="/orgevent">OrganizerEvents</Link>
-          </li>
-          <li>
-              <Link to="/register">Register</Link>
-          </li>
-          <li>
-              <Link to="/card">UserCards</Link>
-          </li>
-          <li>
-              <Link to="/tickt">UserTickets</Link>
-          </li>
-          <li>
-              <Link to="/admin">admin</Link>
-          </li>
-          <li>
-              <Link to="/logout">Logout</Link>
-          </li>
-      </ul>
-      </nav>
+    return (
+        <nav className='navbar'>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/register">Register</Link>
+                </li>
+                <li>
+                    <Link to="/login">Login</Link>
+                </li>
+                <li>
+                    <Link to="/orgdash">OrganizerDashbord</Link>
+                </li>
+                <li>
+                    <Link to="/orgevent">OrganizerEvents</Link>
+                </li>
+                <li>
+                    <Link to="/card">UserCards</Link>
+                </li>
+                <li>
+                    <Link to="/tickt">UserTickets</Link>
+                </li>
+                <li>
+                    <Link to="/admin">admin</Link>
+                </li>
+                <li>
+                    <Link to="/logout">Logout</Link>
+                </li>
+            </ul>
+        </nav>
 
 
-  )
+    )
 }
