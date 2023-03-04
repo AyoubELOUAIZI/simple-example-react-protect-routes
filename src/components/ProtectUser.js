3dd9def51c6a75a8998005463d2fd6e3c9872067
ELOUAIZI AYOUB <ayoubelouaizi@gmail.com>
2023-03-04 20:36:45 +0000

    NavigateBack

--- src/components/ProtectUser.js
+++ src/components/ProtectUser.js
@@ -4,8 +4,10 @@ import AuthContext from './AuthContext';
 
 export const ProtectUser = () => {
     const { profile } = useContext(AuthContext);
-
-    // Allow access for both users and admins
-    ((profile && profile.account) && (profile.account.account_type === "client" || profile.account.account_type === 'admin'))?
-        <Outlet /> : <Navigate to='/login' replace />; 
+    return (
+        <div>Admin
+            {/* // Allow access for both users and admins */}
+            {profile && profile.account && (profile.account.account_type === "client" || profile.account.account_type === 'admin') ?
+                <Outlet /> : <Navigate to='/login' replace />}
+        </div>)
 };
